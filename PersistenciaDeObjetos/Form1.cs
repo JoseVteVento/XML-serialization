81232231f8d46ed0c1486f375205f89644a41d96
Jose Vte Vento <jvvento@gmail.com>
2016-10-23 18:22:48 +0200

    Add User class

--- PersistenciaDeObjetos/Form1.cs
+++ PersistenciaDeObjetos/Form1.cs
@@ -7,45 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml;
-using System.Xml.Serialization;
 
 namespace PersistenciaDeObjetos
 {
+    /// <summary>
+    /// La siguiente clase, es un ejemplo de persistencia de objetos a formato xml
+    /// Para la persistencia, se crea una lista de objetos de tipo User, el cual se almacena 
+    /// en un fichero. 
+    /// Luego, recupera los objetos desde el fichero, y los representa en pantalla    
+    /// </summary>
     public partial class PersistForm : Form
-    {
+    {        
+        private Persist persist;
+
         public PersistForm()
         {
+            persist = new Persist();            
             InitializeComponent();
         }      
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+        private void Form1_Load(object sender, EventArgs e) { }
 
-        }  
-        
         private void bt_persist_Click(object sender, EventArgs e)
         {
-            List <User > lista = new List<User>();
-            decimal  counter = 0;
-            while (counter < 10 )
+            persist.Serializar(tx_nombre.Text, tx_apellidos.Text, tx_telefono.Text, tx_email.Text);
+        }       
+
+        private void bt_toUser_Click(object sender, EventArgs e)
+        {
+            List<User> lista = persist.Deserializar();
+            foreach (User item in lista)
             {
-            //Create e new User using textbox values
-            User m_user = new User(group1.Text,
-                tx_apellidos.Text ,
-                tx_telefono.Text ,
-                tx_email.Text );
-            lista.Add(m_user);
-            counter++;
+                //add the items to the listbox control
+                lsb_listadoUsers.Items.Add(item.nombre + " " + item.apellidos + "  Tlf:" +
+                    item.telefono + "  e-mail:" + item.email);
             }
-
-
-            System.Xml.Serialization.XmlSerializer x =
-                new System.Xml.Serialization.XmlSerializer(lista.GetType());
-
-            x.Serialize(Console.Out, lista);
-            Console.WriteLine();
-            Console.ReadLine();
-        }
+        }        
     }
 }
